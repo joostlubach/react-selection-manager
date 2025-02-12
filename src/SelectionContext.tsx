2b360ce45e7f5c9fc108aae46dce0854cf522aec
Joost Lubach <joostlubach@gmail.com>
2025-02-12 21:21:09 +0100

    Typings.

--- src/SelectionContext.tsx
+++ src/SelectionContext.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SelectionManager from './SelectionManager'
-import { SelectionManagerProviderProps } from './types'
+import { Key, SelectionManagerProviderProps } from './types'
 
 const SelectionContext = React.createContext<SelectionManager<any> | null>(null)
 
@@ -12,10 +12,10 @@ export const SelectionManagerProvider = (props: SelectionManagerProviderProps) =
   )
 }
 
-export function useSelectionManager() {
+export function useSelectionManager<K extends Key>() {
   const context = React.useContext(SelectionContext)
   if (context == null) {
     throw new Error('useSelectionManager must be used within a SelectionManagerProvider')
   }
-  return context
+  return context as SelectionManager<K>
 }
